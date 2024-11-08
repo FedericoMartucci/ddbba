7b930c7513a9f4aaa2fcbaf1421909c010f9c43f
FedericoMartucci <federicomartucci@hotmail.com>
2024-11-08 14:24:09 -0300

    complete merge in the file with reports.

--- 02_Reportes.sql
+++ 02_Reportes.sql
@@ -1,9 +1,15 @@
+--CREAMOS LOS PROCEDURE NECESARIOS PARA LEVANTAR LOS ARCHIVOS QUE RECIBIMOS
+USE Com5600G08
+GO
+
+--CONFIGURACIONES PARA LA IMPORTACION
+-- Habilita la visualización de opciones avanzadas en la configuración del motor
 EXEC sp_configure 'show advanced options', 1;
 RECONFIGURE;
-GO
-EXEC sp_configure 'Ole Automation Procedures', 1;
+--Habilitamos el 'Ole Automation Procedures' para permitir al motor ejecutar comandos de automatización de objetos
+EXEC sp_configure 'Ole Automation Procedures', 1;    
 RECONFIGURE;
-GO
+-- Volvemos a ocultar las opciones avanzadas para evitar cambios accidentales en configuraciones avanzadas
 EXEC sp_configure 'show advanced options', 0;
 RECONFIGURE;
 GO
@@ -326,7 +332,7 @@ BEGIN
         FROM 
             transacciones.VENTA v
         JOIN 
-            aurora.SUCURSAL s ON v.id_sucursal = s.id
+            seguridad.SUCURSAL s ON v.id_sucursal = s.id
         WHERE 
             v.fecha BETWEEN @fechaInicio AND @fechaFin
         GROUP BY 
